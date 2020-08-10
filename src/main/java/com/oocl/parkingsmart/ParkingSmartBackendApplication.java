80b396cca746badee9e3374edbd9f06f7e3ba389
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-10 08:35:29 +0800

    [backend] [henry] feature: config the mysql and SpringBoot

---
+++ src/main/java/com/oocl/parkingsmart/ParkingSmartBackendApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.parkingsmart;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSmartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSmartBackendApplication.class, args);
+    }
+
+}
