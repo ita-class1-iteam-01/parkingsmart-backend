b345ac723d3f2dfd097e0628dcca9e6a6f6d847a
Albert <Albert.c.x.chen@oocl.com>
2020-08-11 13:06:21 +0800

    [ST07][backend][Albert&kiki] feature :add BookSearchServiceTest test

---
+++ src/test/java/com/oocl/parkingsmart/service/BookSearchServiceTest.java
@@ -0,0 +1,15 @@
+package com.oocl.parkingsmart.service;
+
+import org.junit.jupiter.api.Test;
+
+public class BookSearchServiceTest {
+    @Test
+    void should__when__given() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
