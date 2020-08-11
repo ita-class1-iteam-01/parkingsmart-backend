16291568b8df7ea4f8a4b4893675c93e63887bf8
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-11 16:11:47 +0800

    [ST14] [henry] test: add @WebAppConfiguration to BookOrderControllerIntegrationTest

--- src/test/java/com/oocl/parkingsmart/integrationTest/BookOrderControllerIntegrationTest.java
+++ src/test/java/com/oocl/parkingsmart/integrationTest/BookOrderControllerIntegrationTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
@@ -24,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WebAppConfiguration
 public class BookOrderControllerIntegrationTest {
 
     @Autowired
