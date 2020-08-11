bb95d230cc14b338c8f3b882834012a6dc0d6b01
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-11 20:54:34 +0800

    [ST07] [henry] test: remove org.springframework.transaction.annotation.Transactional

--- src/test/java/com/oocl/parkingsmart/integrationTest/BookOrderControllerIntegrationTest.java
+++ src/test/java/com/oocl/parkingsmart/integrationTest/BookOrderControllerIntegrationTest.java
@@ -5,18 +5,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.oocl.parkingsmart.entity.BookOrder;
 import com.oocl.parkingsmart.repository.BookOrderRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.transaction.Transactional;
 import java.util.*;
