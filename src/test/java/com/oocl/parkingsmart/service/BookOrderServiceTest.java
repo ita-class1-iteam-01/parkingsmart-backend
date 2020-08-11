167cf9622bdd0d511bea92af09aa97a0e2b7d07c
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-11 14:26:47 +0800

    [ST14] [henry] feature: implement should_create_book_order_when_create_given_book_order test

--- src/test/java/com/oocl/parkingsmart/service/BookOrderServiceTest.java
+++ src/test/java/com/oocl/parkingsmart/service/BookOrderServiceTest.java
@@ -1,8 +1,10 @@
 package com.oocl.parkingsmart.service;
 
 import com.oocl.parkingsmart.entity.BookOrder;
+import com.oocl.parkingsmart.repository.BookOrderRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -11,13 +13,27 @@ import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class BookOrderServiceTest {
     BookOrderService bookOrderService;
+    BookOrderRepository bookOrderRepository;
 
     @BeforeEach
     void init(){
-        bookOrderService = new BookOrderServiceImpl();
+        bookOrderRepository = Mockito.mock(BookOrderRepository.class);
+        BookOrder order = new BookOrder();
+        order.setParkingId(1);
+        order.setParkingType("Lots");
+        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        calendar.set(2020, Calendar.DECEMBER, 1);
+        order.setReservationStartTime(new Date());
+        order.setReservationEndTime(calendar.getTime());
+        order.setStatus("Finish");
+        order.setId(1);
+        when(bookOrderRepository.save(any())).thenReturn(order);
+        bookOrderService = new BookOrderServiceImpl(bookOrderRepository);
     }
     @Test
     void should_create_book_order_when_create_given_book_order() {
