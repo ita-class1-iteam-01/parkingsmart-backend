38e18343ebe938fee7e461f7ca7bd9e2c886aae1
haohao6683 <852721803@qq.com>
2020-08-12 16:57:12 +0800

    [ST13][Quentin & Draymond] feature: implement should_create_book_order_when_create_personal_order_given_personal_order

--- src/main/java/com/oocl/parkingsmart/service/BookOrderService.java
+++ src/main/java/com/oocl/parkingsmart/service/BookOrderService.java
@@ -1,7 +1,10 @@
 package com.oocl.parkingsmart.service;
 
 import com.oocl.parkingsmart.entity.BookOrder;
+import com.oocl.parkingsmart.entity.RentOrder;
 
 public interface BookOrderService {
     BookOrder create(BookOrder bookOrder);
+
+    BookOrder createPersonalOrder(BookOrder order, RentOrder rentOrder);
 }
--- src/main/java/com/oocl/parkingsmart/service/impl/BookOrderServiceImpl.java
+++ src/main/java/com/oocl/parkingsmart/service/impl/BookOrderServiceImpl.java
@@ -1,21 +1,49 @@
 package com.oocl.parkingsmart.service.impl;
 
 import com.oocl.parkingsmart.entity.BookOrder;
+import com.oocl.parkingsmart.entity.RentOrder;
 import com.oocl.parkingsmart.repository.BookOrderRepository;
+import com.oocl.parkingsmart.repository.RentOrderRepository;
 import com.oocl.parkingsmart.service.BookOrderService;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class BookOrderServiceImpl implements BookOrderService {
-    private final BookOrderRepository bookOrderRepository;
+    private BookOrderRepository bookOrderRepository;
+    private RentOrderRepository rentOrderRepository;
 
     public BookOrderServiceImpl(BookOrderRepository bookOrderRepository) {
         this.bookOrderRepository = bookOrderRepository;
     }
 
+    public BookOrderServiceImpl(RentOrderRepository rentOrderRepository) {
+        this.rentOrderRepository = rentOrderRepository;
+    }
+
+    public BookOrderServiceImpl(BookOrderRepository bookOrderRepository, RentOrderRepository rentOrderRepository) {
+        this.bookOrderRepository = bookOrderRepository;
+        this.rentOrderRepository = rentOrderRepository;
+    }
+
     @Override
     public BookOrder create(BookOrder bookOrder) {
         return bookOrderRepository.save(bookOrder);
+    }
+
+    @Override
+    public BookOrder createPersonalOrder(BookOrder order, RentOrder rentOrder) {
+        if(rentOrder.getStatus().equals("published")){
+            long months = ChronoUnit.MONTHS.between(rentOrder.getRentStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),rentOrder.getRentEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())+1;
+            order.setTotalPrice(months*rentOrder.getPrice());
+            BookOrder returnedOrder = bookOrderRepository.save(order);
+            System.out.println(returnedOrder.getTotalPrice());
+            rentOrder.setStatus("finished");
+            rentOrderRepository.save(rentOrder);
+            return returnedOrder;
+        }
+        return null;
     }
 }
