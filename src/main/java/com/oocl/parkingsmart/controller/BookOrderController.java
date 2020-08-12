7dad366fa432ee9e0a71e65cdca386d5fde21f0b
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-12 19:46:56 +0800

    [ST14] [henry] feature: update the return data when hit post from BookOrderController

--- src/main/java/com/oocl/parkingsmart/controller/BookOrderController.java
+++ src/main/java/com/oocl/parkingsmart/controller/BookOrderController.java
@@ -4,8 +4,10 @@ import com.oocl.parkingsmart.Enum.BookOrderEnum;
 import com.oocl.parkingsmart.entity.BookOrder;
 import com.oocl.parkingsmart.entity.CarSpace;
 import com.oocl.parkingsmart.entity.RentOrder;
+import com.oocl.parkingsmart.model.ParkingLot;
 import com.oocl.parkingsmart.service.BookOrderService;
 import com.oocl.parkingsmart.service.CarSpaceService;
+import com.oocl.parkingsmart.service.ParkingLotService;
 import com.oocl.parkingsmart.utils.ResultVoUtils;
 import com.oocl.parkingsmart.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,6 +28,9 @@ public class BookOrderController {
 
     @Autowired
     CarSpaceService carSpaceService;
+
+    @Autowired
+    ParkingLotService parkingLotService;
 
     @GetMapping
     public ResultVo getAll(){
@@ -44,6 +50,14 @@ public class BookOrderController {
         }
         CarSpace carSpace = unUsedCarSpaces.get(0);
         bookOrder.setCarPort(carSpace.getCarPort());
+        Date reservationStartTime = bookOrder.getReservationStartTime();
+        Date reservationEndTime = bookOrder.getReservationEndTime();
+        long to = reservationEndTime.getTime();
+        long from = reservationStartTime.getTime();
+        int hours = (int)((to-from)/(1000*60*60));
+        ParkingLot parkingLot = parkingLotService.findOne(bookOrder.getParkingId());
+        bookOrder.setTotalPrice(hours*parkingLot.getPrice());
+        bookOrder.setAddress(parkingLot.getAddress());
         if(bookOrderService.create(bookOrder)!=null){
             resultVo.setCode(0);
             resultVo.setMsg("booking success");
