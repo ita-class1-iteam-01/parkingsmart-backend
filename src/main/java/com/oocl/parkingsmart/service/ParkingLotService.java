59852850f6afe67d58d916437ed76f863a3f6182
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-12 16:39:14 +0800

    [ST14] [henry] feature: add and implement should_return_parkingLot_with_remain_when_select__given_parking_lot_id_and_time test

--- src/main/java/com/oocl/parkingsmart/service/ParkingLotService.java
+++ src/main/java/com/oocl/parkingsmart/service/ParkingLotService.java
@@ -2,6 +2,10 @@ package com.oocl.parkingsmart.service;
 
 import com.oocl.parkingsmart.model.ParkingLot;
 
+import java.util.Date;
+
 public interface ParkingLotService {
     ParkingLot findOne(Integer parkingLotId);
+
+    ParkingLot findOneByIdAndStartTimeAndEndTime(Integer parkingLotId, Date startTime, Date endTime);
 }
--- src/main/java/com/oocl/parkingsmart/service/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/oocl/parkingsmart/service/impl/ParkingLotServiceImpl.java
@@ -1,13 +1,42 @@
 package com.oocl.parkingsmart.service.impl;
 
+import com.oocl.parkingsmart.entity.BookOrder;
 import com.oocl.parkingsmart.model.ParkingLot;
+import com.oocl.parkingsmart.repository.ParkingLotRepositoty;
+import com.oocl.parkingsmart.service.BookSearchService;
 import com.oocl.parkingsmart.service.ParkingLotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ParkingLotServiceImpl implements ParkingLotService {
 
+    private final ParkingLotRepositoty repositoty;
+    private final BookSearchService bookSearchService;
+
+    public ParkingLotServiceImpl(ParkingLotRepositoty repositoty,BookSearchService bookSearchService) {
+        this.repositoty = repositoty;
+        this.bookSearchService = bookSearchService;
+    }
 
     @Override
     public ParkingLot findOne(Integer parkingLotId) {
-        return null;
+        return repositoty.findById(parkingLotId).orElse(null);
+    }
+
+    @Override
+    public ParkingLot findOneByIdAndStartTimeAndEndTime(Integer parkingLotId, Date startTime, Date endTime) {
+        Optional<ParkingLot> optional = repositoty.findById(parkingLotId);
+        if(!optional.isPresent()){
+            return null;
+        }
+        ParkingLot parkingLot = optional.get();
+        int usedCarPortCount = bookSearchService.getBookOrdersUsedByStartTimeAndEndTimeAndParkingLotId(parkingLotId, startTime, endTime).size();
+        parkingLot.setSize(parkingLot.getSize()-usedCarPortCount);
+        return parkingLot;
     }
 }
