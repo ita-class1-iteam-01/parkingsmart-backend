9c7a95898a315f71cf7101a7bba1189359b5abfe
haohao6683 <852721803@qq.com>
2020-08-12 16:55:42 +0800

    [ST13][Quentin & Draymond] refactor: modify test

--- src/test/java/com/oocl/parkingsmart/service/BookOrderServiceTest.java
+++ src/test/java/com/oocl/parkingsmart/service/BookOrderServiceTest.java
@@ -1,7 +1,10 @@
 package com.oocl.parkingsmart.service;
 
+import com.oocl.parkingsmart.Enum.RentOrderEnum;
 import com.oocl.parkingsmart.entity.BookOrder;
+import com.oocl.parkingsmart.entity.RentOrder;
 import com.oocl.parkingsmart.repository.BookOrderRepository;
+import com.oocl.parkingsmart.repository.RentOrderRepository;
 import com.oocl.parkingsmart.service.impl.BookOrderServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,26 +22,20 @@ import static org.mockito.Mockito.when;
 class BookOrderServiceTest {
     BookOrderService bookOrderService;
     BookOrderRepository bookOrderRepository;
+    RentOrderRepository rentOrderRepository;
 
     @BeforeEach
-    void init(){
+    void init() {
         bookOrderRepository = Mockito.mock(BookOrderRepository.class);
-        BookOrder order = new BookOrder();
-        order.setParkingId(1);
-        order.setParkingType("Lots");
-        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        calendar.set(2020, Calendar.DECEMBER, 1);
-        order.setReservationStartTime(new Date());
-        order.setReservationEndTime(calendar.getTime());
-        order.setStatus("finish");
-        order.setId(1);
-        when(bookOrderRepository.save(any())).thenReturn(order);
-        bookOrderService = new BookOrderServiceImpl(bookOrderRepository);
+        rentOrderRepository = Mockito.mock(RentOrderRepository.class);
+        bookOrderService = new BookOrderServiceImpl(bookOrderRepository, rentOrderRepository);
     }
+
     @Test
     void should_create_book_order_when_create_given_book_order() {
         //given
         BookOrder order = new BookOrder();
+        order.setId(1);
         order.setParkingId(1);
         order.setParkingType("Lots");
         Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
@@ -46,11 +43,55 @@ class BookOrderServiceTest {
         order.setReservationStartTime(new Date());
         order.setReservationEndTime(calendar.getTime());
         order.setStatus("finish");
+
         //when
+        when(bookOrderRepository.save(any())).thenReturn(order);
         BookOrder bookOrder = bookOrderService.create(order);
+
         //then
         assertNotNull(bookOrder);
         assertNotNull(bookOrder.getId());
-        assertEquals(bookOrder.getParkingId(),order.getParkingId());
+        assertEquals(bookOrder.getParkingId(), order.getParkingId());
+    }
+
+    @Test
+    void should_create_book_order_when_create_personal_order_given_personal_order() {
+        //given
+        BookOrder order = new BookOrder();
+        order.setId(40);
+        order.setParkingId(1);
+        order.setParkingType("personal");
+        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        calendar.set(2019, Calendar.JUNE, 1);
+        order.setReservationStartTime(calendar.getTime());
+        calendar.set(2020, Calendar.SEPTEMBER, 1);
+        order.setReservationEndTime(calendar.getTime());
+        order.setStatus("booked");
+        order.setTotalPrice((double) 200);
+
+        RentOrder rentOrder = new RentOrder();
+        rentOrder.setAddress("广东省珠海市唐家湾惠景畅园");
+        rentOrder.setLatitude(1.1);
+        rentOrder.setLongitude(2.1);
+        rentOrder.setContactNumber("13531915996");
+        rentOrder.setContactPerson("quentin");
+        rentOrder.setPersonCarport("CW-001");
+        rentOrder.setPrice(200.00);
+        rentOrder.setStatus(RentOrderEnum.PUBLISHED.getValue());
+        rentOrder.setUserId(9);
+        calendar.set(2019, Calendar.JUNE, 1);
+        rentOrder.setRentStartDate(calendar.getTime());
+        calendar.set(2020, Calendar.SEPTEMBER, 1);
+        rentOrder.setRentEndDate(calendar.getTime());
+
+        //when
+        when(bookOrderRepository.save(order)).thenReturn(order);
+        BookOrder bookOrder = bookOrderService.createPersonalOrder(order, rentOrder);
+
+        //then
+        assertNotNull(bookOrder);
+        assertNotNull(bookOrder.getId());
+        assertEquals(bookOrder.getParkingId(), order.getParkingId());
+        assertEquals(3200, bookOrder.getTotalPrice());
     }
 }
