2bac96233c1bc87e5fcb5f82ecd9138b1741710f
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-08-13 13:08:53 +0800

    [ST18] [henry] feature:add should_update_rent_order_when_update_given_rent_order test

--- src/main/java/com/oocl/parkingsmart/service/impl/RentOrderServiceImpl.java
+++ src/main/java/com/oocl/parkingsmart/service/impl/RentOrderServiceImpl.java
@@ -33,4 +33,9 @@ public class RentOrderServiceImpl implements RentOrderService {
     public RentOrder findById(Integer id) {
         return rentOrderRepository.findById(id).orElse(null);
     }
+
+    @Override
+    public RentOrder updateRentOrder(RentOrder rentOrder) {
+        return null;
+    }
 }
