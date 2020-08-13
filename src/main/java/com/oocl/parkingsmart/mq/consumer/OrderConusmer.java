be78e454b7fdde9463945d7b8123b9c44dc00d72
haohao6683 <852721803@qq.com>
2020-08-13 16:07:12 +0800

    [ST18][Quentin & Draymond] Feature: modify seckill operation logic

--- src/main/java/com/oocl/parkingsmart/mq/consumer/OrderConusmer.java
+++ src/main/java/com/oocl/parkingsmart/mq/consumer/OrderConusmer.java
@@ -1,23 +1,23 @@
 package com.oocl.parkingsmart.mq.consumer;
 
+import com.google.gson.Gson;
 import com.oocl.parkingsmart.Enum.BookOrderEnum;
-import com.oocl.parkingsmart.Enum.ParkingLotTypeEnum;
 import com.oocl.parkingsmart.Enum.RentOrderEnum;
 import com.oocl.parkingsmart.entity.BookOrder;
 import com.oocl.parkingsmart.entity.RentOrder;
-import com.oocl.parkingsmart.mq.message.OrderMessage;
+import com.oocl.parkingsmart.mq.message.Order;
 import com.oocl.parkingsmart.service.BookOrderService;
 import com.oocl.parkingsmart.service.RentOrderService;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.A;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
-import static com.oocl.parkingsmart.mq.message.OrderMessage.QUEUE;
+import static com.oocl.parkingsmart.mq.message.Order.QUEUE;
 
 @Component
 @Slf4j
@@ -27,34 +27,31 @@ public class OrderConusmer {
     BookOrderService bookOrderService;
     @Autowired
     RentOrderService rentOrderService;
+
     @JmsListener(destination = QUEUE)
-    public void onMessage(OrderMessage message) {
-        //get message, call service method to save order to DB
+    public void onMessage(String message) {
         log.info("get message:{}", message);
         BookOrder bookOrder = new BookOrder();
-        Integer rentId = message.getRentId();
+        Gson gson = new Gson();
+        Order order = gson.fromJson(message,Order.class);
+        Integer rentId = order.getRentId();
         RentOrder rentOrder = rentOrderService.findById(rentId);
         if(rentOrder.getStatus().equals(RentOrderEnum.BOOKED.getValue())){
             return;
         }
-        //copy from rentOrder
-        Date reservationStartTime = bookOrder.getReservationStartTime();
-        Date reservationEndTime = bookOrder.getReservationEndTime();
-        long to = reservationEndTime.getTime();
-        long from = reservationStartTime.getTime();
-        int hours = (int)((to-from)/(1000*60*60));
-        bookOrder.setTotalPrice(hours*rentOrder.getPrice());
+
+        long months = ChronoUnit.MONTHS.between(rentOrder.getRentStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),rentOrder.getRentEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())+1;
+        bookOrder.setTotalPrice(months*rentOrder.getPrice());
         bookOrder.setAddress(rentOrder.getAddress());
-        bookOrder.setParkingType(ParkingLotTypeEnum.PERSONAL.getValue());
+        bookOrder.setParkingType("personal");
         bookOrder.setStatus(BookOrderEnum.BOOKED.getValue());
         bookOrder.setCarPort(rentOrder.getPersonCarport());
-        bookOrder.setUserId(message.getUserId());
+        bookOrder.setUserId(order.getUserId());
         bookOrder.setReservationStartTime(rentOrder.getRentStartDate());
         bookOrder.setReservationEndTime(rentOrder.getRentEndDate());
         bookOrder.setParkingId(rentOrder.getId());
         if(bookOrderService.create(bookOrder)!=null){
-            //update
-            rentOrder.setStatus(RentOrderEnum.PUBLISHED.getValue());
+            rentOrder.setStatus(RentOrderEnum.BOOKED.getValue());
             rentOrderService.updateRentOrder(rentOrder);
             return;
         }
